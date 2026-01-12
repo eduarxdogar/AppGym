6fda743a6726c8994181b0fa9cc5e4e571aab396
eduarxdogar <cristiangarzon1224@hotmail.com>
2026-01-11 22:54:51 -0500

    feat(core): integrate firebase firestore and refactor workout service to signals

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+   apiKey: "AIzaSyDch-B8yd8SE7Aok2RDDwSrssEHyvCmryE",
+  authDomain: "appgym-d8061.firebaseapp.com",
+  projectId: "appgym-d8061",
+  storageBucket: "appgym-d8061.firebasestorage.app",
+  messagingSenderId: "980746465984",
+  appId: "1:980746465984:web:2bf3baa030454f46b3b325",
+  measurementId: "G-K38DLFSRJH"
+  },
+  geminiApiKey: "AIzaSyAYBNFfeS6EllUPk1365NG7X0qFqqXrRi0"
+};
